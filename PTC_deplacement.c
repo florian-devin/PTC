b71c666f9a569da46fdbea37cbe50acc604a5f1b
florian <florian.devin13@gmail.com>
2021-03-04 15:22:24 +0100

    Implementation des fonction Avancer et Stop

---
+++ PTC_deplacement.c
@@ -0,0 +1,30 @@
+//------------------------------------------------------
+// Date: <DATE>
+// Heure: <TIME>
+// Nom/Prénom: Devin florian
+// Email: <florian.devin@cpe.fr>
+// Nom Module: PTC_deplacement.c
+// Description: Fonctions de deplacement elementaires 
+//------------------------------------------------------
+
+#include "PTC_deplacement.h"
+#include "PTC_strOperateurs.h"  // pour my_strcat
+#include "PTC_UART.h" // pour Send_str_uart1
+
+
+
+
+void Avancer(char *str_vitesse){
+    char data[64] = "mogo 1:";
+	my_strcat(data,str_vitesse);
+	my_strcat(data," 2:");
+	my_strcat(data,str_vitesse);
+	my_strcat(data,"\r");
+	Send_str_uart1("\r"); //remet a 0 le serializer
+	Send_str_uart1(data); //evoie du message
+}
+
+void Stop(void){
+    Send_str_uart1("\r"); //remet a 0 le serializer
+	Send_str_uart1("stop\r");
+}
---
+++ PTC_deplacement.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------
+// Date: <DATE>
+// Heure: <TIME>
+// Nom/Prénom: Devin florian
+// Email: <florian.devin@cpe.fr>
+// Nom Module: PTC_deplacement.h
+// Description: Fonctions de deplacement elementaires 
+//------------------------------------------------------
+
+//Demmande au serializer d'avancer a la vitesse str_vitesse
+//@param vitesse sous forme d'un char *
+//@return rien
+void Avancer(char *str_vitesse);
+
+//Demmande au serializer darreter les moteurs
+//@param rien
+//@return rien
+void Stop(void);
