2f9df739c02c9f1c8c8fd79a3c9bd04c106bb11d
florian <florian.devin13@gmail.com>
2021-03-06 23:56:56 +0100

    continue la fonction go_coordinates

--- PTC_deplacement.c
+++ PTC_deplacement.c
@@ -11,7 +11,7 @@
 #include "PTC_deplacement.h"
 #include "PTC_strOperateurs.h"  // pour my_strcat
 #include "PTC_UART.h" // pour Send_str_uart1
-
+#include "PTC_math.h" //pour go_coordinates
 
 
 
@@ -29,8 +29,8 @@ void Stop(void){
     Send_str_uart1("\r"); //remet a 0 le serializer
 	Send_str_uart1("stop\r");
 }
-
 /*
+
 void turn_right(int angle){
 	//TODO
 }
@@ -48,13 +48,18 @@ short teta_angle = 0;//angle des coordonnees cible
 
 
 int go_coordinates(int coord_x, int coord_y){
-	if (flag_calcule_angle == 1) {
+	if (flag_calcule_angle == 1) 
 		teta_angle = my_atan((coord_x - x_robot)/(coord_y - y_robot));
+	else {
+		if (can_go(10)){
+
+		}
 	}
+	
+
 
 
 
 }
-
-
 */
+
