3cfc8eae8880f851abedcd0cc480d8ced1859d61
florian-devin <florian.devin13@gmail.com>
2021-04-12 11:59:48 +0200

    Ajout des fonction accuse de reception pour le Slave

---
+++ 8051Slave/PTC_accuseDeReception_SPI.c
@@ -0,0 +1,20 @@
+//------------------------------------------------------
+// Date: <DATE>
+// Heure: <TIME>
+// Nom/Prénom: Devin florian
+// Email: <florian.devin@cpe.fr>
+// Nom: PTC_accuseDeReception_SPI.c
+// Destination: 8150F020 Carte Slave
+// Description: Fonctions qui gere les accuses de receptions
+//------------------------------------------------------
+#include "PTC_accuseDeReception_SPI.h"
+#include "PTC_SPI.h"
+#include "SPI_RingBuffer_Slave.h"
+
+void AR_cmd_incorrecte_SPI(){//envoi de #
+    serOutchar(0x23);
+}
+
+void AR_cmd_correcte_SPI(){ //envoi de > 
+    serOutchar(0x3E);
+}
